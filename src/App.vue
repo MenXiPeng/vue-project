f9ead3695e2e53c4d5dba7b1778c916360ab9200
menxipeng@zhongbaounion.com <menxipeng@zhongbaounion.com>
2023-02-28 09:29:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <NavVue />
+  </header>
+
+  <main>
+
+  </main>
+</template>
+<script setup>
+import NavVue from './components/Nav.vue'
+
+</script>
+
+<style scoped>
+
+</style>
